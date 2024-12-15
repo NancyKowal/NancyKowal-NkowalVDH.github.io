c25f428dcf5c8c807ab2b9c80dc76d1b1fc3dc3c
Nancy Kowal <nancy.kowal@hotmail.be>
2024-12-15 17:07:07 +0100

    open nav

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const btnNavEl = document.querySelector(".mobile-nav-btn");
+const headerEl = document.querySelector("header")
+
+btnNavEl.addEventListener('click', function(){
+    headerEl.classList.toggle('nav-open')
+}
+);
